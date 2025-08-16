29fb45bcc9917baac3349fb68c32cb9d712e5d80
AmirOmidid <omidi.amir@gmail.com>
2025-08-16 12:31:38 -0400

    The CLI wrapper for train.py prepared!

---
+++ scripts/trian_model.py
@@ -0,0 +1,19 @@
+# scripts/train_model.py
+import argparse
+from kws.training.train import train_model
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("--data_root", default="data")
+    p.add_argument("--epochs", type=int, default=3)
+    p.add_argument("--batch_size", type=int, default=128)
+    p.add_argument("--lr", type=float, default=1e-3)
+    p.add_argument("--n_mfcc", type=int, default=40)
+    p.add_argument("--n_classes", type=int, default=10)
+    p.add_argument("--num_workers", type=int, default=2)
+    args = p.parse_args()
+
+    train_model(**vars(args))
+
+if __name__ == "__main__":
+    main()
